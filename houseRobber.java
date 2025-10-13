cb71bea79664784b685fde8e88826505d1d05e99
Santosh Degala <degalasantosh@gmail.com>
2025-10-13 23:23:46 +0100

    dp sum-house robber

---
+++ houseRobber.java
@@ -0,0 +1,21 @@
+public class houseRobber{
+    public int rob(int[] nums) {
+        if(nums == null || nums.length == 0)
+            return 0;
+        if(nums.length == 1)
+            return nums[0];
+        int n = nums.length;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for(int i = 2; i < n; i++){
+            dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[n - 1];
+    }
+    public static void main(String[] args) {
+        houseRobber solver = new houseRobber();
+        int[] houses = {2,7,9,3,1};
+        System.out.println(solver.rob(houses)); // Output: 12
+    }
+}
